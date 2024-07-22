6b523030cdad4672963efa9245e4291e125970f4
luciansurdu66 <luciansurdu66@gmail.com>
2024-07-22 11:43:23 +0300

    Created basic app structure + routes

---
+++ todo/src/pages/ToDo/ListPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TodoListPage = () => {
+  return (
+    <div>
+      <h2>Welcome, TodoList!</h2>
+      <p>This is the Home page. You have access to standard user features.</p>
+    </div>
+  );
+};
+
+export default TodoListPage;
