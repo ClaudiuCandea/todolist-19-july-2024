fa8abc945b0d09fe9eea292b2565736d53cdbc44
Daniel Alexandru Trif <d.trif@fme.ro>
2024-07-24 13:05:19 +0300

    Updated List Page

--- todo/src/pages/ToDo/List/ListPage.jsx
+++ todo/src/pages/ToDo/List/ListPage.jsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { IoMdAdd } from "react-icons/io";
+import ToDoItem from "../../../components/ToDoItem/ToDoItem";
 
 const TodoListPage = () => {
-  return (
-    <div>
-      <h2>Welcome, TodoList!</h2>
-      <p>This is the Home page. You have access to standard user features.</p>
-    </div>
-  );
+    const [todos, setTodos] = useState([]);
+
+    const fetchToDos = async () => {
+        try {
+            const response = await fetch("http://localhost:3001/todos");
+            if (response.ok) {
+                const json = await response.json();
+                setTodos(json);
+            }
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
+    useEffect(() => {
+        fetchToDos();
+    }, []);
+
+
+    return (
+        <div className="container mx-auto p-6">
+            <div className="flex flex-col items-start gap-8 mb-6">
+                <h2 className="text-3xl">Todo List</h2>
+                <button
+                    className="text-white font-bold bg-blue-500 transition duration-200 hover:bg-blue-300 px-4 py-2 rounded flex items-center"
+                >
+                    <IoMdAdd className="text-xl" />
+                    Add Task
+                </button>
+            </div>
+            <ul className="todo-list grid grid-cols-1 xl:grid-cols-2 gap-4">
+                {todos.map((todo) => (
+                    <li className="todo-item md:flex" key={todo.id}>
+                        <ToDoItem todo={todo} />
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default TodoListPage;
