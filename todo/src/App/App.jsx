13408806cca2ef5a57dc26d7cafd9c2e7ef728e1
Zoltan Vasile Szabo <z.szabo@fme.ro>
2024-07-25 12:20:26 +0300

    pages linked to home + small changes

--- todo/src/App/App.jsx
+++ todo/src/App/App.jsx
@@ -1,16 +1,16 @@
-import '../App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import React from 'react';
-import TodoListPage from '../pages/ToDo/List/ListPage';
-import TodoUpdatePage from '../pages/ToDo/Update/UpdatePage';
-import TodoCreatePage from '../pages/ToDo/Create/CreatePage';
-import { Toaster } from 'react-hot-toast';
-import Welcome from '../pages/Home/Welcome';
+import "../App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from "react";
+import TodoListPage from "../pages/ToDo/List/ListPage";
+import TodoUpdatePage from "../pages/ToDo/Update/UpdatePage";
+import TodoCreatePage from "../pages/ToDo/Create/CreatePage";
+import { Toaster } from "react-hot-toast";
+import Home from "../pages/Home/Home";
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Welcome paths={[]} />} />
+        <Route path="/" element={<Home />} />
         <Route path="/todo" element={<TodoListPage />} />
         <Route path="/todo/:id" element={<TodoUpdatePage />} />
         <Route path="/todo/create" element={<TodoCreatePage />} />
