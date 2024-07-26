de494df2d033e64c30d409dfde64e08b527301e1
Surdu Lucian <luciansurdu66@gmail.com>
2024-07-26 10:19:36 +0300

    Modified Welcome to Home

--- todo/src/App/App.jsx
+++ todo/src/App/App.jsx
@@ -6,7 +6,7 @@ import TodoListPage from '../pages/ToDo/List/ListPage';
 import TodoUpdatePage from '../pages/ToDo/Update/UpdatePage';
 import TodoCreatePage from '../pages/ToDo/Create/CreatePage';
 import { Toaster } from 'react-hot-toast';
-import Welcome from '../pages/Home/Welcome';
+import Home from '../pages/Home/Home';
 import LoginPage from '../pages/Login/GoogleLogin';
 import { useAuth } from '../Hooks/useAuth';
 
@@ -20,7 +20,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/login' element={<LoginPage />} />
-        <Route path="/home" element={isAuthenticated ? <Welcome paths={[]} /> : <Navigate to="/login"/>} />
+        <Route path="/home" element={isAuthenticated ? <Home paths={[]} /> : <Navigate to="/login"/>} />
         <Route path="/todo" element={isAuthenticated ? <TodoListPage /> : <Navigate to="/login" />} />
         <Route path="/todo/:id" element={isAuthenticated ? <TodoUpdatePage /> : <Navigate to="/login" />} />
         <Route path="/todo/create" element={isAuthenticated ? <TodoCreatePage /> : <Navigate to="/login" />} />
