5b7c9e0656a593deca3e7052a990b5e281a20a87
Claudiu Candea <c.candea@fme.ro>
2024-07-26 13:01:10 +0300

    useMemo added in chart component

--- todo/src/components/PieChart.jsx
+++ todo/src/components/PieChart.jsx
@@ -1,22 +1,24 @@
 import { Chart } from "react-google-charts";
-import React from "react";
+import React, { useMemo } from "react";
+
+
+const processTodos = (todos) => {
+  const itemCounter = (value, index) => {
+    return value.filter((x) => x === index).length;
+  };
+  const catergories = todos.map((todo) => {return todo.category})
+  const setCategories = new Set(catergories)
+  const catArr = Array.from(setCategories)
+  const data = catArr.map((todo) => {
+    return [todo, itemCounter(catergories,todo)]
+  })
+  data.unshift(["Todo", "Numbers for each category"])
+  return data
+}
 
 
 const PieChart = ({todos}) => {
-
-    const itemCounter = (value, index) => {
-        return value.filter((x) => x === index).length;
-    };
-
-    const catergories = todos.map((todo) => {return todo.category})
-    const setCategories = new Set(catergories)
-    const catArr = Array.from(setCategories)
-    const data = catArr.map((todo) => {
-        return [todo, itemCounter(catergories,todo)]
-    })
-    data.unshift(["Todo", "Numbers for each category"])
-    console.log(data)
-
+    const data = useMemo( () => processTodos(todos),[todos])
     return(
       <div>
         <Chart
